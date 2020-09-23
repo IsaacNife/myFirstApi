69f4c37cc524fc0fc2578af4c9c1c75e42310351
Isaac Ndjepel <ndjepelfred@gmail.com>
2020-09-23 15:48:34 -0400

    data storage

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 const config = require('./config');
 const fs = require('fs');
+const _data = require('./lib/data');
+
+// Testing 
+//@TODO delete this
+_data.delete('test','newFile',function(err){
+	console.log(err);
+})
 
 // Instantiating the HTTP server
 const httpServer = http.createServer(function(req,res){
